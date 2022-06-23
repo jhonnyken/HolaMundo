79aff1c6a8877249891e23a72ec793c1b9b6ef49
Sergio Noriega <sergio.noriega@valid.com>
2022-06-23 08:10:12 -0500

    Se agrego un comentario de pull

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -14,7 +14,7 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Acabaste de hacer un pull de los cambios");
     }
     
 }
